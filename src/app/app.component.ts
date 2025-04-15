f8ead589da0681b511ef98c0bce09ff40076109c
Md Fahim <mdfahimhossen629@gmail.com>
2025-04-15 22:42:48 +0600

    Update animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
+  ngOnInit() {
+    AOS.init({
+      duration: 1000, // animation duration
+      once: true      // only animate once
+    });
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => AOS.init())
   .catch(err => console.error(err));
